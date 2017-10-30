d31d1e8d72bd5c7780e71f50b466ceacbdeadb64
cvluca <z5155740@unsw.edu.au>
2017-10-30 12:43:49 +1100

    add arrows

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 
 # Left Opt + q => Esc
 ln -s $PWD/Esc.json ~/.config/karabiner/assets/complex_modifications/1.json
+
+ln -s $PWD/Arrows.json ~/.config/karabiner/assets/complex_modifications/2.json
