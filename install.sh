8eedcedca8a883e2c220862cc2cb30bf393c9895
cvluca <z5155740@unsw.edu.au>
2017-12-04 11:39:33 +1100

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!bin/sh
 
 # Left Option+q => Esc
+rm ~/.config/karabiner/assets/complex_modifications/1.json
 ln -s $PWD/Esc.json ~/.config/karabiner/assets/complex_modifications/1.json
 
 # Left Option+h/j/k/l => Arrows
+rm ~/.config/karabiner/assets/complex_modifications/2.json
 ln -s $PWD/Arrows.json ~/.config/karabiner/assets/complex_modifications/2.json
