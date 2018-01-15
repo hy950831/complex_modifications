d788d2249388857689ef1424acc584dca6b4fc88
cvluca <z5155740@unsw.edu.au>
2018-01-15 11:12:36 +1100

    add Launch

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ ln -s $PWD/Esc.json ~/.config/karabiner/assets/complex_modifications/1.json
 # Left Option+h/j/k/l => Arrows
 rm ~/.config/karabiner/assets/complex_modifications/2.json
 ln -s $PWD/Arrows.json ~/.config/karabiner/assets/complex_modifications/2.json
+
+# Launch new apps window
+rm ~/.config/karabiner/assets/complex_modifications/3.json
+ln -s $PWD/Launch.json ~/.config/karabiner/assets/complex_modifications/3.json
